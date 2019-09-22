f46d825070ab6a68f70a0071d742a6d213ed510c
DNI9 <itsr9boi@gmail.com>
2019-09-22 23:51:04 +0530

    Update shrp_env.sh

--- shrp_env.sh
+++ shrp_env.sh
@@ -10,4 +10,9 @@ export SHRP_REC="/dev/block/platform/mtk-msdc.0/by-name/recovery"
 
 export SHRP_FLASH="1"
 export SHRP_FONP_1="echo 16 > /sys/class/leds/torch/brightness"
+export SHRP_FONP_2=""
+export SHRP_FONP_3=""
+
 export SHRP_FOFP_1="echo 0 > /sys/class/leds/torch/brightness"
+export SHRP_FOFP_2=""
+export SHRP_FOFP_3=""
