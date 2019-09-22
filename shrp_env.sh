0567e549a13b7b04de1e0a4d954dc8862876a2fc
DNI9 <itsr9boi@gmail.com>
2019-09-22 23:23:24 +0530

    shrp : k3 note specificc values

---
+++ shrp_env.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+export SHRP_MAINTAINER="DNI9"
+export SHRP_DEVICE="aio_otfp"
+export SHRP_EDL_MODE="0"
+export SHRP_EXTERNAL="/external_sd"
+export SHRP_INTERNAL="/sdcard"
+export SHRP_OTG="/usb-otg"
+export SHRP_REC="/dev/block/platform/mtk-msdc.0/by-name/recovery"
+
+export SHRP_FLASH="1"
+export SHRP_FONP_1="echo 16 > /sys/class/leds/torch/brightness"
+export SHRP_FOFP_1="echo 0 > /sys/class/leds/torch/brightness"
